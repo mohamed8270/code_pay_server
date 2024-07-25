628ba552576208af717de6262947aa96b604d5dd
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-07-25 10:46:48 +0530

    email notification and server action updates are made

--- models/job.model.js
+++ models/job.model.js
@@ -3,30 +3,30 @@ const mongoose = require('mongoose');
 const jobSchema = new mongoose.Schema({
     url: {type: String, required: true, unique: true},
     jobName: {type: String, required: true},
-    jobCompany: {type: String, required: true},
-    jobPlace: {type: String, required: true},
-    jobExperience: {type: String, required: true},
-    jobSalary: {type: String, required: true},
-    jobPosted: {type: String, required: true},
-    jobViews: {type: String, required: true},
-    jobApplied: {type: String, required: true},
+    jobCompany: {type: String, required: false},
+    jobPlace: {type: String, required: false},
+    jobExperience: {type: String, required: false},
+    jobSalary: {type: String, required: false},
+    jobPosted: {type: String, required: false},
+    jobViews: {type: String, required: false},
+    jobApplied: {type: String, required: false},
     jobDescription: {type: String, required: true},
-    jobType: {type: String, required: true},
-    jobIndustry: {type: String, required: true},
-    jobFunction: {type: String, required: true},
+    jobType: {type: String, required: false},
+    jobIndustry: {type: String, required: false},
+    jobFunction: {type: String, required: false},
     jobRole: [
-        {type: String, required: true},
+        {type: String, required: false},
     ],
     jobSkillsets: [
-        {type: String, required: true},
+        {type: String, required: false},
     ],
-    jobCompanyUrl: {type: String, required: true},
-    jobSource: {type: String, required: true},
+    jobCompanyUrl: {type: String, required: false},
+    jobSource: {type: String, required: false},
     jobApply: {type: String, required: true},
-    jobQuickApply: {type: String, required: true},
-    jobPostedValue: {type: Number, required: true},
-    jobAppliedValue: {type: Number, required: true},
-    jobViewsValue: {type: Number, required: true},
+    jobQuickApply: {type: String, required: false},
+    jobPostedValue: {type: Number, required: false},
+    jobAppliedValue: {type: Number, required: false},
+    jobViewsValue: {type: Number, required: false},
     postedHistory: [
         {
             posted: {type: Number, required: true},
@@ -36,6 +36,12 @@ const jobSchema = new mongoose.Schema({
     appliedHistory: [
         {
             apply: {type: Number, required: true},
+            Date: {type: Date, default: Date.now},
+        }
+    ],
+    viewsHistory: [
+        {
+            views: {type: Number, required: true},
             Date: {type: Date, default: Date.now},
         }
     ],
